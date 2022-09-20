f477807bcec96bca16f2bf48b78e23d42117c8ac
Daryoush Washburn <fizzio11111@gmail.com>
2022-09-20 00:45:56 -0700

    osu framework stuff

---
+++ CrankItUp/CrankItUp.Desktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using osu.Framework.Platform;
+using osu.Framework;
+using CrankItUp.Game;
+
+namespace CrankItUp.Desktop
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (GameHost host = Host.GetSuitableDesktopHost(@"CrankItUp"))
+            using (osu.Framework.Game game = new CrankItUpGame())
+                host.Run(game);
+        }
+    }
+}
