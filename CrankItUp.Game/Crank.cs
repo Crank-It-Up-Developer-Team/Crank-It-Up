001351415cff5d850c82f31a30fc4858b678b574
AnnoyingRains <Annoyingrain5@gmail.com>
2022-09-21 11:55:56 +1000

    Add file extension to file

---
+++ CrankItUp.Game/Crank.cs
@@ -0,0 +1,68 @@
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Graphics.Cursor;
+using osuTK;
+using System;
+
+
+namespace CrankItUp.Game{
+    public class Crank: CompositeDrawable{
+        public Crank(){
+            AutoSizeAxes = Axes.Both;
+            Origin = Anchor.Centre;
+        }
+
+        private Container box;
+        private CursorContainer mouse;
+
+        private void load(TextureStore textures){
+
+            mouse = new CursorContainer();
+            mouse.ToggleVisibility();
+             
+             InternalChild = box = new Container
+            {
+                AutoSizeAxes = Axes.Both,
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Children = new Drawable[]
+                {
+                    new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                    },
+                    new Sprite
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Texture = textures.Get("crank")
+                    },
+                }
+            
+            };
+        }
+
+        protected override void LoadComplete(){
+            Vector2 pos = mouse.Position;
+            float direction = (float)Math.Atan2(pos.Y, pos.X);
+            box.RotateTo(direction, 1000);
+            base.LoadComplete();
+            
+        }
+
+        
+
+        
+
+        
+
+        
+
+    }
+}
