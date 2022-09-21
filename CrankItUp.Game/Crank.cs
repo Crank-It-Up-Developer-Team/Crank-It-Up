495dd834b9f9dd50dd4c33fdc970189320d6362f
AnnoyingRains <Annoyingrain5@gmail.com>
2022-09-21 15:12:47 +1000

    Load depenencies and use Update() for rotation

--- CrankItUp.Game/Crank.cs
+++ CrankItUp.Game/Crank.cs
@@ -9,9 +9,12 @@ using osuTK;
 using System;
 
 
-namespace CrankItUp.Game{
-    public class Crank: CompositeDrawable{
-        public Crank(){
+namespace CrankItUp.Game
+{
+    public class Crank : CompositeDrawable
+    {
+        public Crank()
+        {
             AutoSizeAxes = Axes.Both;
             Origin = Anchor.Centre;
         }
@@ -19,18 +22,20 @@ namespace CrankItUp.Game{
         private Container box;
         private CursorContainer mouse;
 
-        private void load(TextureStore textures){
+        [BackgroundDependencyLoader]
+        private void load(TextureStore textures)
+        {
 
             mouse = new CursorContainer();
             mouse.ToggleVisibility();
-             
-             InternalChild = box = new Container
+
+            InternalChild = box = new Container
             {
                 AutoSizeAxes = Axes.Both,
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
                 Children = new Drawable[]
-                {
+               {
                     new Box
                     {
                         RelativeSizeAxes = Axes.Both,
@@ -43,26 +48,20 @@ namespace CrankItUp.Game{
                         Origin = Anchor.Centre,
                         Texture = textures.Get("crank")
                     },
-                }
-            
+               }
+
             };
         }
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+        }
 
-        protected override void LoadComplete(){
+        protected override void Update()
+        {
             Vector2 pos = mouse.Position;
             float direction = (float)Math.Atan2(pos.Y, pos.X);
             box.RotateTo(direction, 1000);
-            base.LoadComplete();
-            
         }
-
-        
-
-        
-
-        
-
-        
-
     }
 }
