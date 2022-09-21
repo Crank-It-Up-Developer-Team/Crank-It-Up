c5387bf060e72d689b79cef0c05f592447bd0697
AnnoyingRains <Annoyingrain5@gmail.com>
2022-09-21 11:20:51 +1000

    Added Program.cs to .Desktop to allow project to compile

---
+++ CrankItUp.Desktop/Program.cs
@@ -0,0 +1,27 @@
+﻿using osu.Framework.Platform;
+using osu.Framework;
+using CrankItUp.Game;
+
+namespace CrankItUp.Game
+{
+    public static class Program
+    {
+        private static osu.Framework.Game game = new CrankItUpGame();
+        private static  GameHost host = Host.GetSuitableDesktopHost(@"CrankItUp");
+        public static void Main()
+        {
+            using (host)
+            using (game)
+                host.Run(game);
+        }
+
+        public static CrankItUpGame getGame(){
+            return (CrankItUpGame)game;
+        }
+
+        public static GameHost GetGameHost(){
+            return host;
+        }
+
+    }
+}
