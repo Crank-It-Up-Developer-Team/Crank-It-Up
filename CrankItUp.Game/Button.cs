1bb99ce505d5b7c91031932d893f074a7a067e74
AnnoyingRain5 <AnnoyingRain5@gmail.com>
2022-11-09 11:03:19 +0700

    Remove unused using statements

--- CrankItUp.Game/Button.cs
+++ CrankItUp.Game/Button.cs
@@ -1,18 +1,14 @@
-using osu.Framework.Allocation;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
-using osu.Framework.Input.Events;
 using osu.Framework.Localisation;
-using osuTK;
 using osu.Framework.Graphics.UserInterface;
 namespace CrankItUp.Game
 {
     public class CIUButton : Button
     {
         TextureStore texturestore;
-        
+
         public LocalisableString Text // Most of this is from BasicButton
         {
             get => text?.Text ?? default;
@@ -22,14 +18,15 @@ namespace CrankItUp.Game
                     text.Text = value;
             }
         }
-        
+
         public Texture Texture;
         public Colour4 TextColour = Colour4.White;
 
         private Sprite background;
         private SpriteText text;
 
-        public CIUButton(TextureStore texturestore){
+        public CIUButton(TextureStore texturestore)
+        {
             if (Texture == null)
             {
                 Texture = texturestore.Get("Button");
