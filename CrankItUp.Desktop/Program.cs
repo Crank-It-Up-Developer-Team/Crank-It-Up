7b3a644b9ea852a7265c8ef197b66c81ded8e091
AnnoyingRains <annoyingrain5@gmail.com>
2022-11-09 17:59:43 +1100

    Added Discord Rich Presence

--- CrankItUp.Desktop/Program.cs
+++ CrankItUp.Desktop/Program.cs
@@ -2,15 +2,18 @@
 using osu.Framework;
 using CrankItUp.Game;
 using System;
+using CrankItUp.Desktop;
 
 namespace CrankItUp.Game
 {
     public static class Program
     {
         private static osu.Framework.Game game = new CrankItUpGame();
-        private static  GameHost host = Host.GetSuitableDesktopHost(@"CrankItUp");
+        private static GameHost host = Host.GetSuitableDesktopHost(@"CrankItUp");
+        public static Discord discordrpc = new Discord();
         public static void Main()
         {
+            discordrpc.Initialize();
             using (host)
             using (game)
                 host.Run(game);
